f41b229a6395fb14a608a497827a27d0593a438b
D'ontreye Nero <dontreyenero@gmail.com>
2016-12-12 22:57:05 -0500

    initial find first sum commit

---
+++ findFirstSum.js
@@ -0,0 +1,14 @@
+/*
+ Given a list of integers and a single sum value, return the first two values (parsing from the left) in order of appearance that add up to form the sum.
+ */
+
+sum_pairs = function(ints, s){
+	var result = [];
+
+
+	return result;
+}
+
+
+// sum_pairs([1, 4, 8, 7, 3, 15], 8) // => [1, 7]
+// sum_pairs([4, -2, 3, 3, 4], 8) // => [4, 4]
