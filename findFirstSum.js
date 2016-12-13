a886b01d4de67e5f954d70b5dfd063821c58d4da
D'ontreye Nero <dontreyenero@gmail.com>
2016-12-12 23:39:10 -0500

    nested loop to find first instance of sum

--- findFirstSum.js
+++ findFirstSum.js
@@ -3,12 +3,22 @@
  */
 
 sum_pairs = function(ints, s){
-	var result = [];
+	var result = [],
+		i,
+		j,
+		len = ints.length;
 
-
-	return result;
+	for(i = 0; i < len; i++){
+		 for(j = 0; j < len; j++){
+			if(ints[i] + ints[j] === s){
+				result.push(ints[i], ints[j]);
+				return result;
+			}
+		 }
+	}
 }
 
 
-// sum_pairs([1, 4, 8, 7, 3, 15], 8) // => [1, 7]
-// sum_pairs([4, -2, 3, 3, 4], 8) // => [4, 4]
+//console.log('answer: ', sum_pairs([1, 4, 8, 7, 3, 15], 8)); // => [1, 7]
+//console.log('answer: ', sum_pairs([1, -2, 3, 0, -6, 1], -6)); // => [0, -6]
+console.log('answer: ', sum_pairs([10, 5, 2, 3, 7, 5], 10)); // => [3, 7]
