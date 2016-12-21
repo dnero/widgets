e1c387caa2c8dcacd1022026eef4433a4e0ea95c
D'ontreye Nero <dontreyenero@gmail.com>
2016-12-21 14:37:51 -0500

    add case for exponential numbers

--- latLongValidator.js
+++ latLongValidator.js
@@ -1,5 +1,5 @@
 function isValidCoordinates(coordinates){
-	var latLong = coordinates.split(','),
+	var latLong = coordinates.split(',')
 		lat = Number(latLong[0]),
 		long = Number(latLong[1]);
 
@@ -7,6 +7,7 @@ function isValidCoordinates(coordinates){
 		( !/^[-]?(\d)+[.]?(\d)*/.test(lat) || !/^[-]?(\d)+[.]?(\d)*/.test(long) ) ||
 		( latLong.length > 2 ) ||
 		( isNaN(lat) || isNaN(long) ) ||
+		(coordinates.toLowerCase().includes('e') ) ||
 		( Math.abs(lat) > 90 || Math.abs(long) > 180 )
 	){
 		return false;
