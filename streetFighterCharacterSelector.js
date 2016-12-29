981df57765919fe7265de09805d8d89802b3b8f1
D'ontreye Nero <dontreyenero@gmail.com>
2016-12-29 02:14:52 -0500

    Refactor Street Fighter logic for perfomance

--- streetFighterCharacterSelector.js
+++ streetFighterCharacterSelector.js
@@ -1,35 +1,34 @@
 // Return the path of characters hovered based on a list of directions run.
 // wrapping works horizontally, but not vertically
+// http://jsben.ch/#/1g55c
 
 let streetFighterSelection = (fighters, position, moves) => {
 
 	let row = position[0], // row
 		col = position[1], // column
-		fightersHovered = []; // list of fighters that have been hovered over
-	
+		fightersHovered = [], // list of fighters that have been hovered over
+		rowLength = fighters[0].length - 1;
+		
 	moves.forEach( (move) => {
 		'use strict';
 
-		if (move === 'up' && row !== 0) { // trying to move up and not on top row
-			row--;
+		if (move === 'up') {
+			row = Math.max(row - 1, 0);
 
-		} else if (move === 'down' && row < fighters.length - 1) {
-			row++;
-			console.log('row++', row);
+		} else if (move === 'down') {
+			row = Math.min(row + 1, 1);
 
 		} else if (move === 'left') {
-			col = (col === 0) ? fighters[0].length - 1 : col - 1;
-			
+			col = (col === 0) ? rowLength : col - 1;
+
 		} else if (move === 'right') {
-			col = (col === fighters[0].length - 1) ? 0 : col + 1;
+			col = (col + 1) % 6;
 		}
 		
 		fightersHovered.push(fighters[row][col]);
-		console.log(`Row: ${row}, Col: ${col}`);
-		console.log(fighters[row][col]);
-		console.log('fightersHovered: ', fightersHovered);
 	});
-	
+
+	return fightersHovered;
 };
 
 let fighters = [
@@ -38,5 +37,5 @@ let fighters = [
 	],
 	moves;
 
-// moves = ['up','left','down','right','up','left','down','right'];
-// streetFighterSelection(fighters, [1, 0], moves); // should return ['Ryu', 'Vega', 'M.Bison', 'Ken', 'Ryu', 'Vega', 'M.Bison', 'Ken']);
+moves = ['up','left','down','right','up','left','down','right'];
+streetFighterSelection(fighters, [1, 0], moves); // should return ['Ryu', 'Vega', 'M.Bison', 'Ken', 'Ryu', 'Vega', 'M.Bison', 'Ken']);
